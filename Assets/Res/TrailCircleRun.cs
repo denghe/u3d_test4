e53cb45184b6e01200d3295f7491fa0336a9f703
denghe <denghe@hotmail.com>
2024-08-23 18:48:43 +0800

    finish trail effect

---
+++ Assets/Res/TrailCircleRun.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailCircleRun : MonoBehaviour
+{
+    public float distance = 1;
+    public float radiansSpeed = Mathf.PI / 180;
+    public float radians;
+    private Vector3 posBak;
+
+    void Start()
+    {
+        posBak = transform.position;
+    }
+
+    private void FixedUpdate() {
+        radians += radiansSpeed;
+        if (radians > Mathf.PI) {
+            radians -= Mathf.PI * 2;
+        }
+        var cos = Mathf.Cos(radians);
+        var sin = Mathf.Sin(radians);
+        transform.position = new Vector3(
+            posBak.x + cos * distance,
+            posBak.y + sin * distance,
+            posBak.z);
+    }
+}
